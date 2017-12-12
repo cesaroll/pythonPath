8f52d7b1944ef78702f6d1e06e2d9eb0a6b2a583
Cesar Lopez <cesarl@outlook.com>
2017-12-12 16:55:16 +0000

    Batteries included for iteration, itertools islice and count

---
+++ pythonFundamentals/08-Iterables/iteration.py
@@ -0,0 +1,19 @@
+import math
+from itertools import islice, count
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+def sumPrimes(maxNum):
+    return sum(islice((x for x in count() if is_prime(x)), maxNum) )
+
+def run_sumPrimes():
+    print(sumPrimes(1000))
+
+if __name__ == '__main__':
+    run_sumPrimes()
