0954430e2147383301a88e89b6d7a81f5e0397e9
Cesar Lopez <cesarl@outlook.com>
2017-12-12 20:46:00 +0000

    Initializers

---
+++ pythonFundamentals/09-Classes/airtravel.py
@@ -0,0 +1,26 @@
+""" Model for aircraft flights """
+
+class FligthEmpty:
+    pass # Do nothing pass statement to be syntactically admisible
+
+class Flight:
+    
+    def __init__(self, number):
+        
+        if not number[:2].isalpha():
+            raise ValueError("No airline code in '{}'".format(number))
+            
+        if not number[:2].isupper():
+            raise ValueError("Invalid airline code '{}'".format(number))
+            
+        if not (number[2:].isdigit() and int(number[2:]) <= 9999):
+            raise ValueError("Invalid route number '{}'".format(number))
+        
+        self._number = number
+    
+    def number(self):
+        return self._number
+        
+    def airline(self):
+        return self._number
+        
