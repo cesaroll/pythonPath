7d6a7899443daa60a7ccf246f7ecac96816ffa20
Cesar Lopez <cesarl@outlook.com>
2017-12-13 21:47:06 +0000

    OO with function Objects - print boarding pass

--- pythonFundamentals/09-Classes/airtravel.py
+++ pythonFundamentals/09-Classes/airtravel.py
@@ -100,6 +100,19 @@ class Flight:
     def num_available_seats(self):
         return sum(sum(1 for s in row.values() if s is None)
                     for row in self._seating if row is not None)
+                    
+    def make_boarding_cards(self, card_printer):
+        for passenger, seat in sorted(self._passenger_seats()):
+            card_printer(passenger, seat, self.number(), self.aircraft_model())
+    
+    def _passenger_seats(self):
+        """An iterable series of passenger seating allocations."""
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+        for row in row_numbers:
+            for letter in seat_letters:
+                passenger = self._seating[row][letter]
+                if passenger is not None:
+                    yield (passenger, "{}{}".format(row, letter))
 
 class Aircraft:
     
@@ -132,6 +145,21 @@ def make_flight():
     
     return f
 
+
+def console_card_printer(passenger, seat, flight_number, aircraft):
+    output =    "| Name: {0}"\
+                "  Flight: {1}"\
+                "  Seat: {2}"\
+                "  Aircraft: {3}"\
+                " |".format(passenger, flight_number, seat, aircraft)
+    banner = '+' + '-' * (len(output) - 2) + '+'
+    border = '|' + ' ' * (len(output) - 2) + '|'
+    lines = [banner, border, output, border, banner]
+    card = '\n'.join(lines)
+    print(card)
+    print()
+
+
 if __name__ == '__main__':
     try:
         f = make_flight()
@@ -139,6 +167,8 @@ if __name__ == '__main__':
         pp(f._seating)
         
         print("\nAvalable seats: {}\n".format(f.num_available_seats()))
+        
+        f.make_boarding_cards(console_card_printer)
     except ValueError as e:
         print(e)
     
