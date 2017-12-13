2e1ef98e109b5753f65d7967a156177327d43749
Cesar Lopez <cesarl@outlook.com>
2017-12-13 22:27:45 +0000

    Polymorphism, inheritance and implementation

--- pythonFundamentals/09-Classes/airtravel.py
+++ pythonFundamentals/09-Classes/airtravel.py
@@ -116,23 +116,37 @@ class Flight:
 
 class Aircraft:
     
-    def __init__(self, registration, model, num_rows, num_seats_per_row):
+    def __init__(self, registration):
         self._registration = registration
-        self._model = model
-        self._num_rows = num_rows
-        self._num_seats_per_row = num_seats_per_row
     
     def registration(self):
         return self._registration
         
-    def model(self):
-        return self._model
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len(row_seats)
+   
         
-    def seating_plan(self): #returns tuple (rows, seats)
-        return (range(1, self._num_rows + 1), "ABCDEFGHJK"[:self._num_seats_per_row])
+class AirbusA319(Aircraft):
+    
+    def model(self):
+        return "Airbus A319"
+        
+    def seating_plan(self):
+        return  range(1, 23), "ABCDEF"
+        
+class Boeing777(Aircraft):
 
+    def model(self):
+        return "Boeing777"
+        
+    def seating_plan(self):
+        return  range(1, 23), "ABCDEFGHJK"
+        
+        
+        
 def make_flight():
-    f = Flight("BA758", Aircraft("G-EUPT", "Airbus A319", num_rows=22, num_seats_per_row=6) )
+    f = Flight("BA758", AirbusA319("G-EUPT") )
     f.allocate_seat('12A', 'Guido Van Rossum')
     #f.allocate_seat('12A', 'Rasmus Lerdorf')
     f.allocate_seat('15F', 'Bjarne Stroustrup')
